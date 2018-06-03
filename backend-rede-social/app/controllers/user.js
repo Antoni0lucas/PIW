7ae926ed231ec59bd288e1acccff6c3de97d9c8c
Antoni0lucas <31545672+Antoni0lucas@users.noreply.github.com>
2018-06-03 15:57:29 -0300

    atividade 8

---
+++ backend-rede-social/app/controllers/user.js
@@ -0,0 +1,52 @@
+let users = [{ _id: 1, nome: "É o preno", email: "nogueirao@email.com", senha: "luise" },
+{ _id: 2, nome: "Dhesquinha", email: "dhesquinha@email.com", senha: "barro" }];
+
+module.exports.listarUsers = function (req, res) {
+    res.json(users);
+};
+
+module.exports.obterUser = function (req, res) {
+    let id = req.params.id;
+    let user = users.find(user => (user._id == id));
+    if (user) {
+        res.json(user);
+    }
+    else {
+        res.status(404).send('Usuario não encontrado');
+    }
+};
+
+module.exports.inserirUser = function (req, res) {
+    users.push(req.body);
+    res.status(200).send(req.body);
+    console.log(req.body);
+}
+
+module.exports.deletarUser = function (req, res) {
+    let id = req.params.id;
+    let user = users.find(user => (user._id == id));
+    if (user) {
+        let i = users.indexOf(user);
+        if (i != -1) {
+            users.splice(i, 1);
+        }
+        res.json(user);
+    }
+    else {
+        res.status(404).send('Usuario não encontrado');
+    }
+};
+
+module.exports.updateUser = function (req, res) {
+    let id = req.params.id;
+
+}
+
+module.exports.retornaPosts = function (req, res) {
+    if (req.query.min_id) {
+        let list = posts.filter((el) => (el._id >= req.query.min_id));
+        res.json(list);
+    } else {
+        res.json(posts);
+    }
+}
